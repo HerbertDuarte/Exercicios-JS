86f6891e6fecab24f39925b50a2c66b75903c1c6
HerbertDuarte <herbertduarte.santos@gmail.com>
2022-12-08 02:00:20 -0300

    08-12-22

---
+++ ex010(repeticoes)/ambiente.js
@@ -0,0 +1,15 @@
+var c = 0
+
+while(c < 4){
+  console.log('oi, boa noite!')
+  c++
+}
+
+/*
+ao invés de fazer : 
+
+console.log('oi monote')
+console.log('oi monote')
+console.log('oi monote')
+console.log('oi monote')
+ */
